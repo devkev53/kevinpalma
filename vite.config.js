3e2eac304b1ecf63a55b4aeef38f2a5101bcc27f
Devkev53 <kapalma05@gmail.com>
2026-05-23 15:09:40 -0600

    🧪 Test change in base vite.config
    test change in base vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // base: "https://devkev53.github.io/kevinpalma",
+  base: "https://devkev53.github.io/",
 });
